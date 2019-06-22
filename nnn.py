a2b5908cd17da324dad3fc76ebebe47a9805a7d4
jesusble <keziahblessy33@gmail.com>
2019-06-22 16:24:09 +0530

    Update nnn.py

--- nnn.py
+++ nnn.py
@@ -1,7 +1,7 @@
-nu1=int(input())
+nu9=int(input())
 k1=list(map(int,input().split()))
-s1=[1]*nu1
-for i in range(nu1):
+s1=[1]*nu9
+for i in range(nu9):
     if i==0:
         if k1[i]>k1[i+1]:
             s1[i]=s1[i]+s1[i+1]
