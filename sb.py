060282a27e7323b15cda1fbeffcd720ff579b068
jesusble <keziahblessy33@gmail.com>
2019-06-22 16:27:26 +0530

    Update sb.py

--- sb.py
+++ sb.py
@@ -1,11 +1,11 @@
-s1,s2=input().split()  
-s1=int(s1)  
-s2=int(s2)   
+sb1,sb2=input().split()  
+sb1=int(sb1)  
+sb2=int(sb2)   
 v=list(map(int,input().split()))
 count=0  
 for i in range(len(v)):
   for j in range(i+1,len(v)):
-    if (v[i]+v[j]==s2):
+    if (v[i]+v[j]==sb2):
       count+=1
       break
 if(count):
